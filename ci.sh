dc25b20fc00ff4d618eae439939ccebb47478ce3
GUO YANKE <hi@yankeguo.com>
2024-09-03 15:58:09 +0800

    refactor: update ci.sh for new directory naming

--- ci.sh
+++ ci.sh
@@ -11,12 +11,12 @@ case "$1" in
         exit 1
     fi
 
-    echo "context=${ADDR[0]}:${ADDR[1]}"
+    echo "context=${ADDR[0]}--${ADDR[1]}"
     echo "name=${ADDR[0]}"
     echo "tag=${ADDR[1]}-${ADDR[2]}"
     ;;
 "create-tag")
-    TAG_NAME="${2//:/--}--$(date '+%y%m%d-%H%M')"
+    TAG_NAME="${2}--$(date '+%y%m%d-%H%M')"
     git tag -m "$TAG_NAME" "$TAG_NAME"
     git push --tags origin main:main
     ;;
