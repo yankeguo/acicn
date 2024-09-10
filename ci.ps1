45511cc136cff6f1cb6412e0f782c087032aa1a0
GUO YANKE <hi@yankeguo.com>
2024-09-10 09:58:48 +0800

    fix(ci): fix ci.ps1 tagging

--- ci.ps1
+++ ci.ps1
@@ -3,8 +3,9 @@ Param(
 )
 
 if ($CreateTag) {
+    $CreateTag.Trim(".", " ", "/")
     $Suffix = Get-Date -Format "yyyyMMdd-HHmm"
-    $Tag = "$CreateTag-$Suffix"
+    $Tag = "$CreateTag--$Suffix"
     git tag -m "$Tag" "$Tag"
     git push --tags origin main:main
 }
